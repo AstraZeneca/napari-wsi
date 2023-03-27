2797b42f3ac1d201dcfd909f190ef2336adbe9c7
Philipp Plewa <philipp.plewa@astrazeneca.com>
2023-03-27 15:41:20 +0200

    Make image series selectable

--- napari_wsi/widget.py
+++ napari_wsi/widget.py
@@ -20,18 +20,20 @@ DEFAULT_BACKEND = WSIReaderBackend.tifffile
     call_button="Load",
     path={"label": "Path", "tooltip": "The file path."},
     backend={"label": "Backend", "tooltip": "The backend for reading WSI data."},
+    series={"label": "Series", "tooltip": "The image series to read."},
     split_rgb={"label": "Split RGB", "tooltip": "If set, split 3-channel images."},
 )
 def get_wsi_reader_widget(
     path: Path,
     backend: WSIReaderBackend = DEFAULT_BACKEND,
+    series: int = 0,
     split_rgb: bool = False,
 ) -> List[LayerDataTuple]:
     if not path.is_file():
         raise FileNotFoundError(path)
 
     if backend == WSIReaderBackend.tifffile:
-        return read_tifffile(path, split_rgb=split_rgb)
+        return read_tifffile(path, series=series, split_rgb=split_rgb)
     if backend == WSIReaderBackend.rasterio:
         return read_rasterio(path, split_rgb=split_rgb)
 
